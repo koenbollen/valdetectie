531c05f68b57015f1acebd9bee2186c95286e954
bollen05 <bollen05@aacf9060-d2d2-4c4e-b042-a2c323390b44>
2009-09-16 13:50:48 +0000

    Added VideoFileImageStream and the tool to view;
    git-svn-id: http://oege.ie.hva.nl/svn/valdetectie2009/trunk@7 aacf9060-d2d2-4c4e-b042-a2c323390b44

---
+++ valdetectie/src/com/tigam/valdetectie/streams/VideoFileImageStream.java
@@ -0,0 +1,65 @@
+package com.tigam.valdetectie.streams;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+/**
+ * An implementation of {@link ImageStream} to capture frames from a webcam in
+ * linux
+ * 
+ * @author Koen Bollen
+ */
+public class VideoFileImageStream implements ImageStream
+{
+
+	public final File file;
+
+	private InputStream in;
+	
+	public VideoFileImageStream(File file) throws ImageStreamException, IOException
+	{
+		this (file, 1024, 768);
+	}
+
+	public VideoFileImageStream(File file, int width, int height) throws ImageStreamException, IOException
+	{
+
+		this.file = file;
+
+		if( !file.exists() )
+			throw new IOException("No such file or directory.");
+
+		try
+		{
+			String resolution = width+"x"+height;
+			String command = "ffmpeg -i " + file + " -s "+resolution+" -f image2pipe -vcodec bmp -";
+
+			Process cam = Runtime.getRuntime().exec(command);
+			in = cam.getInputStream();
+		} catch( IOException ball )
+		{
+			throw new ImageStreamException("Unable to start ffmpeg.", ball);
+		} catch( SecurityException ball )
+		{
+			throw new ImageStreamException("Unable to start ffmpeg.", ball);
+		}
+
+	}
+
+	@Override
+	public synchronized Image read()
+	{
+		try
+		{
+			return ImageIO.read(this.in);
+		} catch( IOException e )
+		{
+			return null;
+		}
+	}
+
+}
---
+++ valdetectie/src/com/tigam/valdetectie/tools/ViewVideoFile.java
@@ -0,0 +1,47 @@
+package com.tigam.valdetectie.tools;
+
+import java.awt.Image;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import com.tigam.valdetectie.streams.GrayScaleImageStream;
+import com.tigam.valdetectie.streams.VideoFileImageStream;
+import com.tigam.valdetectie.streams.ImageStream;
+import com.tigam.valdetectie.utils.Imager;
+
+public class ViewVideoFile
+{
+
+	public static void main(String[] args)
+	{
+		File file;
+		
+		final JFileChooser fc = new JFileChooser();
+
+		int res = fc.showOpenDialog(null);
+		if( res != JFileChooser.APPROVE_OPTION )
+			System.exit(1);
+		file = fc.getSelectedFile();
+		
+		ImageStream str = null;
+		try
+		{
+			str = new VideoFileImageStream(file);
+		} catch( Exception e )
+		{
+			System.err.println( "unable to openfile: " + e.getMessage() );
+			System.exit(0);
+		}
+		Imager imager = new Imager();
+		imager.setVisible(true);
+		while (true){
+			Image img = str.read();
+			if (img == null)
+				break;
+			imager.setImage(img);
+		}
+		System.exit(0);
+	}
+
+}
