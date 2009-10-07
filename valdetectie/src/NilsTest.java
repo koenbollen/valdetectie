f5644a910d5838935ae4fa4a1842bcb070ee24a5
bollen05 <bollen05@aacf9060-d2d2-4c4e-b042-a2c323390b44>
2009-10-07 14:38:34 +0000

    Changed the structure of the BackgroundModel so each phase of the model generation has its own methode Started with the use of the BackgroundModel to create a foreground mask
    git-svn-id: http://oege.ie.hva.nl/svn/valdetectie2009/trunk@24 aacf9060-d2d2-4c4e-b042-a2c323390b44

--- valdetectie/src/NilsTest.java
+++ valdetectie/src/NilsTest.java
@@ -1,30 +1,42 @@
 import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import com.tigam.valdetectie.algorithms.BackgroundModel;
 import com.tigam.valdetectie.streams.ImageStream;
 import com.tigam.valdetectie.streams.VideoFileImageStream;
+import com.tigam.valdetectie.streams.filters.GrayScaleImageStream;
 import com.tigam.valdetectie.utils.Imager;
 import com.tigam.valdetectie.utils.Utils;
 
+public class NilsTest
+{
+	public static void main(String... args) throws Exception
+	{
+		ImageStream in = new VideoFileImageStream(new File(
+				"/home/public/hall_monitor.mpg"));
+		in = new GrayScaleImageStream(in);
 
-public class NilsTest {
-	public static void main(String...args) throws Exception {
-		ImageStream in = new VideoFileImageStream(new File("/home/public/hall_monitor.mpg"));
-		
 		Imager img1 = new Imager();
 		img1.setVisible(true);
-		
+
 		Imager img2 = new Imager();
 		img2.setVisible(true);
-		
-		BackgroundModel bm = new BackgroundModel(in.width(),in.height());
-		while (true){
-			int [] data1 = in.read();
-			if (data1 == null) break;
-			int [] data2 = bm.pushImage(data1);
-			
+
+		BackgroundModel bm = new BackgroundModel(in.width(), in.height());
+
+		while( true )
+		{
+			int[] data1 = in.read();
+			if( data1 == null )
+				break;
+			bm.pushImage(data1);
+			bm.refreshBackgroundModel();
 			img1.setImage(Utils.data2image(data1, in.width(), in.height()));
-			img2.setImage(Utils.data2image(data2, in.width(), in.height()));
+			img2.setImage(Utils.data2image(bm.getForeground(data1), in.width(), in.height()));
+
 		}
 	}
 }
--- valdetectie/src/com/tigam/valdetectie/algorithms/BackgroundModel.java
+++ valdetectie/src/com/tigam/valdetectie/algorithms/BackgroundModel.java
@@ -1,4 +1,5 @@
 package com.tigam.valdetectie.algorithms;
+
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
@@ -11,31 +12,36 @@ import com.tigam.valdetectie.utils.PixelHistory;
 public class BackgroundModel
 {
 	private final int width;
+
 	private final int height;
+
 	private final int length;
-	
+
 	private PixelHistory ph;
-	
+
 	private DynamicMedian[] median;
+
 	private MeanDeviation[] diviation;
 
 	// The model:
 	private int[] m;
+
 	private int[] n;
+
 	private int[] d;
-	
-	public BackgroundModel( int width, int height )
+
+	public BackgroundModel(int width, int height)
 	{
 		this(width, height, 100);
 	}
-	
-	public BackgroundModel( int width, int height, int period )
+
+	public BackgroundModel(int width, int height, int period)
 	{
 		this.width = width;
 		this.height = height;
-		this.length = width*height;
-		
-		period = max(1, period%2==0 ? period-1 : period ); 
+		this.length = width * height;
+
+		period = max(1, period % 2 == 0 ? period - 1 : period);
 		ph = new PixelHistory(period, this.length);
 
 		this.median = new DynamicMedian[this.length];
@@ -45,68 +51,110 @@ public class BackgroundModel
 			this.median[i] = new DynamicMedian();
 			this.diviation[i] = new MeanDeviation();
 		}
-		
+
 		// Initialize the Model:
 		this.m = new int[this.length];
 		this.n = new int[this.length];
 		this.d = new int[this.length];
 	}
-	
-	public int[] pushImage( int[] frame )
-	{
-		int[] old = ph.insert(frame);//this.history.insert(frame);		
-		return update(frame, old);
-		
-	}
 
-	private int[] update( int[] data, int[] fold )
-	{
-		
-		int[] buffer = new int[this.length];
 
-		Arrays.fill(this.m, Integer.MAX_VALUE);
-		Arrays.fill(this.n, Integer.MIN_VALUE);
-		Arrays.fill(this.d, Integer.MIN_VALUE);
+	public void pushImage(int[] frame)
+	{
+		int[] old = ph.insert(frame);// this.history.insert(frame);
 
 		for( int i = 0; i < this.length; i++ )
 		{
 			int pixel;
-			pixel = data[i] & 0xFF;
-			
-			// Calculate Median:
-			if( fold != null )
-				this.median[i].remove(fold[i] & 0xFF);
-			this.median[i].insert(pixel);
-			
-			// Calculate Mean and Standard Deviation:
-			if( fold != null )
-				this.diviation[i].remove(fold[i] & 0xFF);
-			this.diviation[i].insert( pixel );
+			pixel = frame[i] & 0xFF;
 
-			buffer[i] = (int)this.diviation[i].deviation();
-			buffer[i] = 0xFF000000 | buffer[i] << 16 | buffer[i] << 8 | buffer[i];
+			// Calculate Median:
+			if( old != null )
+				this.median[i].remove(old[i] & 0xFF);
+			this.median[i].insert(pixel);
+
+			// Calculate Mean and Standard Deviation:
+			if( old != null )
+				this.diviation[i].remove(old[i] & 0xFF);
+			this.diviation[i].insert(pixel);
 		}
-		
+	}
+	
+	public int[] getDeviationImage()
+	{
+		int[] buffer = new int[this.length];
 		for( int i = 0; i < this.length; i++ )
 		{
-			int [] pixelHistory = ph.getPixelHistory(i);
-			for( int k = 1; k < pixelHistory.length; k++ )
-			{
-				int pixelData = pixelHistory[k] & 0xFF;
-
-				double q = pixelData - this.median[i].median();
-				q /= 2.0;
-				q = q*q;
-				
-				if( q <= this.diviation[i].deviationSquared() )
-				{
-					m[i] = min( m[i], pixelData );
-					n[i] = max( n[i], pixelData );
-					d[i] = max( d[i], Math.abs(pixelData-(pixelHistory[k-1]&0xFF)) );
-				}
-			}
-		}		
+			buffer[i] = (int) this.diviation[i].deviation();
+			buffer[i] = 0xFF000000 | buffer[i] << 16 | buffer[i] << 8 | buffer[i];
+		}
 		return buffer;
 	}
 	
+	public void refreshBackgroundModel(){
+		Arrays.fill(this.m, Integer.MAX_VALUE);
+		Arrays.fill(this.n, Integer.MIN_VALUE);
+		Arrays.fill(this.d, Integer.MIN_VALUE);
+		
+		int size = ph.size();
+		for( int i = 0; i < this.length; i++ )
+		{
+			int[] pixelHistory = ph.getPixelHistory(i);
+
+			double median = this.median[i].median();
+			double dvs = this.diviation[i].deviationSquared();
+
+			for( int k = 1; k < size; k++ )
+			{
+				int pixelData = pixelHistory[k] & 0xFF;
+
+				double q = pixelData - median;
+				q /= 2;
+				q = q * q;
+
+				if( q <= dvs ){
+					m[i] = min(m[i], pixelData);
+					n[i] = max(n[i], pixelData);
+					d[i] = max(d[i], Math.abs(pixelData	- (pixelHistory[k - 1] & 0xFF)));
+				}
+			}
+		}
+	}
+	
+	public int[] getBackgroundImage(){
+		int [] image = new int[length];
+		for (int i=0; i<length; i++){
+			int pix = (m[i]+n[i])/2;
+			image[i] = 0xFF000000 | pix << 16 | pix << 8 | pix;
+		}
+		return image;
+	}
+	
+	public int[] getForeground(int [] image){
+		if (image.length != length) throw new RuntimeException("size of imagge does not fit the size of the background");
+		int back = 0xFF000000;
+		int fore = 0xFFFFFFFF;
+		
+		int [] foreground = new int[length];
+		
+		Arrays.fill(foreground, fore);
+		
+		DynamicMedian median = new DynamicMedian();
+		for (int t:d){
+			try {
+				median.insert(t);
+			} catch (IndexOutOfBoundsException ball) {}
+		}
+		
+		int k = 2;
+		int u = median.median();
+		
+		for (int i=0; i<length; i++){
+			int pix = image[i]&0xFF;
+			if (pix > (m[i]-k*u) && pix < (n[i]+k*u)) foreground[i] = back;
+		}
+		
+		return foreground;
+	}
+
 }
