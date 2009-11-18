8b6a0a4888091f76b4dd876988c7d7f2f7e9299d
dijk8w <dijk8w@aacf9060-d2d2-4c4e-b042-a2c323390b44>
2009-11-18 14:26:12 +0000

    Created an ImageStream to drop frames
    git-svn-id: http://oege.ie.hva.nl/svn/valdetectie2009/trunk@36 aacf9060-d2d2-4c4e-b042-a2c323390b44

---
+++ valdetectie/src/com/tigam/valdetectie/streams/DropImageStream.java
@@ -0,0 +1,60 @@
+package com.tigam.valdetectie.streams;
+
+public final class DropImageStream implements ImageStream {
+
+	final class Poller implements Runnable {
+
+		@Override
+		public void run() {
+			while (!Thread.interrupted()) {
+				int[] data = source.read();
+				synchronized (mutex) {
+					image = data;
+					mutex.notifyAll();
+				}
+			}
+		}
+
+	}
+
+	protected final ImageStream source;
+
+	protected int[] image;
+	protected final Object mutex;
+
+	public DropImageStream(ImageStream source) {
+		this.source = source;
+		this.mutex = new Object();
+
+		Thread t = new Thread(new Poller());
+		t.setName("Frame Dropper");
+		t.start();
+	}
+
+	@Override
+	public int height() {
+		return this.source.height();
+	}
+
+	@Override
+	public int[] read() {
+		int[] data;
+		synchronized (mutex) {
+			try {
+				while (image == null)
+					mutex.wait();
+			} catch (InterruptedException ball) {
+				return null;
+			}
+			data = image;
+			image = null;
+		}
+		return data;
+	}
+
+	@Override
+	public int width() {
+		return this.source.height();
+	}
+
+}
